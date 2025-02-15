ee7b40639125d59c3d2a9205e30d1b469f9968a1
Ascherya <ascherya_cse@mvsrec.edu.in>
2025-02-15 11:35:37 +0530

    1st commit

--- sample.java
+++ sample.java
@@ -1,1 +1,7 @@
-This  is about jenkins
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print the message to the console
+        System.out.println("Hello, World!");
+    }
+}
+
